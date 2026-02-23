eb209c0b785e09ee2557dd7107a91285721604f5
Aimene Hamidechi <aymen.hamidechi@gmail.com>
2026-02-23 05:06:19 +0100

    fsfg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { useEffect } from 'react';
-import { useAppDispatch } from './store/hooks';
-import { fetchCampaigns } from './store/campaignsSlice';
-import { fetchFilterDefinitions } from './store/filtersSlice';
-import { useFilterPersistence } from './hooks/useFilterPersistence';
-import { Header } from './components/Header/Header';
-import { CampaignList } from './components/CampaignList/CampaignList';
-import { SidePanel } from './components/SidePanel/SidePanel';
-import styles from './App.module.css';
+import { useEffect, useMemo } from "react";
+import { useAppDispatch } from "./store/hooks";
+import { fetchCampaigns } from "./store/campaignsSlice";
+import { fetchFilterDefinitions } from "./store/filtersSlice";
+import { useFilterPersistence } from "./hooks/useFilterPersistence";
+import { Header } from "./components/Header/Header";
+import { CampaignList } from "./components/CampaignList/CampaignList";
+import { SidePanel } from "./components/SidePanel/SidePanel";
+import styles from "./App.module.css";
 
 function App() {
   const dispatch = useAppDispatch();
@@ -17,8 +17,18 @@ function App() {
     dispatch(fetchFilterDefinitions());
   }, [dispatch]);
 
+  const testFunction = (test: string) => {
+    console.log(test);
+  };
+
+  const testMemo = useMemo(() => {
+    testFunction("hello");
+    return 1;
+  }, [testFunction]);
+
   return (
     <div className={styles.layout}>
+      {testMemo}
       <Header />
       <main className={styles.main}>
         <CampaignList />
